4581f807b1f76a53c0cd8b0f27f9b9027158dba8
GY <13620366368@163.com>
2019-12-11 09:57:19 +0800

    已完成

--- script/index.js
+++ script/index.js
@@ -32,68 +32,68 @@ class Tab {
 new Tab('#tab')
 
 // 列表请求
-// $(function () {
-//   $.ajax({
-//       url: 'http://127.0.0.1:5000/get_all_msg',
-//       dataType: 'json',
-//       success: function (data) {
-//         console.log(data);
-//         for (let value of data) {
-//           console.log(value);
-//           let $cloneli = $('.list').first().clone();
-//           $cloneli.find('.Head-portrait').attr('src', value.avatar);
-//           $cloneli.find('.liName').html(value.title);
-//           $cloneli.find('.time').html(value.time);
-//           $cloneli.appendTo('.lists ul')
-//           $cloneli.click(function () {
-//             console.log(value.detail);
-//             let $clonedetails = $('.details').first().clone();
-//             $clonedetails.find('.details-img').attr('src', value.avatar);
-//             $clonedetails.find('.details-title').html(value.title);
-//             $clonedetails.find('.details-name').html(value.author);
-//             $clonedetails.find('.detail-content').html(value.detail);
-//             $clonedetails.find('.detail-time').html(value.time);
-//             $clonedetails.appendTo('.In-the')
-//             $('.Modal-dialog').show();
-//             $('.Details-page').show();
-//           })
-//         }
-//       }
-//     }),
-//     // 提交按钮
-//     $('#btnclick').click(function () {
-//       if ($('#nickname').val() != '' && $('#title').val() != '' && $('#text').val() != '' && $('#img ').val() != '') {
-//         console.log('全部有值');
-//         // 发送请求
-//         $.ajax({
-//           url: 'http://127.0.0.1:5000/add_msg',
-//           data: {
-//             title: $('#nickname').val(),
-//             author: $('#title').val(),
-//             detail: $('#text').val(),
-//             avatar: $('#img ').val()
-//           },
-//           success(data) {
-//             console.log(data);
-//           }
-//         })
-//       } else {
-//         $('.Modal-dialog').show();
-//         $('.prompt').show();
-//       }
-
-//     })
-//   $('.determine').click(function () {
-//     $('.Details-page').hide();
-//     $('.Modal-dialog').hide();
-//     $('.In-the').empty();
-//     $('.prompt').hide();
-//   })
-// })
-
-
-
 $(function () {
+  $.ajax({
+      url: 'http://127.0.0.1:5000/get_all_msg',
+      dataType: 'json',
+      success: function (data) {
+        console.log(data);
+        for (let value of data) {
+          console.log(value);
+          let $cloneli = $('.list').first().clone();
+          $cloneli.find('.Head-portrait').attr('src', value.avatar);
+          $cloneli.find('.liName').html(value.title);
+          $cloneli.find('.time').html(value.time);
+          $cloneli.appendTo('.lists ul')
+          $cloneli.click(function () {
+            console.log(value.detail);
+            let $clonedetails = $('.details').first().clone();
+            $clonedetails.find('.details-img').attr('src', value.avatar);
+            $clonedetails.find('.details-title').html(value.title);
+            $clonedetails.find('.details-name').html(value.author);
+            $clonedetails.find('.detail-content').html(value.detail);
+            $clonedetails.find('.detail-time').html(value.time);
+            $clonedetails.appendTo('.In-the')
+            $('.Modal-dialog').show();
+            $('.Details-page').show();
+          })
+        }
+      }
+    }),
+    // 提交按钮
+    $('#btnclick').click(function () {
+      if ($('#nickname').val() != '' && $('#title').val() != '' && $('#text').val() != '' && $('#img ').val() != '') {
+        console.log('全部有值');
+        // 发送请求
+        $.ajax({
+          url: 'http://127.0.0.1:5000/add_msg',
+          data: {
+            title: $('#nickname').val(),
+            author: $('#title').val(),
+            detail: $('#text').val(),
+            avatar: $('#img ').val()
+          },
+          success(data) {
+            console.log(data);
+          }
+        })
+      } else {
+        $('.Modal-dialog').show();
+        $('.prompt').show();
+      }
+
+    })
+  $('.determine').click(function () {
+    $('.Details-page').hide();
+    $('.Modal-dialog').hide();
+    $('.In-the').empty();
+    $('.prompt').hide();
+  })
+  // })
+
+
+
+  // $(function () {
   $("#search_frame").hide();
   $.ajax({
     url: 'https://api.asilu.com/weather/',
@@ -127,6 +127,7 @@ $(function () {
         $cloneweatherli.find('.weather-img').attr('src', './' + value.icon2 + '.png'); //状态图
         $cloneweatherli.find('.temperature').html(value.temp); //温度
         $cloneweatherli.appendTo('.weather')
+        console.log(value.icon2);
 
       }
       $('.weather .day').first().html('今天');
@@ -178,49 +179,49 @@ $(function () {
 // 更新背景方法
 function EstimateWeatherimg(weather) {
   if (weather === '晴') {
-    $('.instrument').css("background-image", "url(./images/天气_晴.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_晴.jpg)");
   } else if (weather === '多云转晴') {
-    $('.instrument').css("background-image", "url(./images/天气_晴.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_晴.jpg)");
   } else if (weather === '雾转晴') {
-    $('.instrument').css("background-image", "url(./images/天气_晴.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_晴.jpg)");
   } else if (weather === '小雪转晴') {
-    $('.instrument').css("background-image", "url(./images/天气_晴.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_晴.jpg)");
   } else if (weather === '小雨') {
-    $('.instrument').css("background-image", "url(./images/天气_下雨.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_下雨.jpg)");
   } else if (weather === '暴雨') {
-    $('.instrument').css("background-image", "url(./images/天气_下雨.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_下雨.jpg)");
   } else if (weather === '阴转小雨') {
-    $('.instrument').css("background-image", "url(./images/天气_下雨.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_下雨.jpg)");
   } else if (weather === '阴') {
-    $('.instrument').css("background-image", "url(./images/天气_阴天.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_阴天.jpg)");
   } else if (weather === '多云转阴') {
-    $('.instrument').css("background-image", "url(./images/天气_阴天.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_阴天.jpg)");
   } else if (weather === '晴转阴') {
-    $('.instrument').css("background-image", "url(./images/天气_阴天.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_阴天.jpg)");
   } else if (weather === '晴转多云') {
-    $('.instrument').css("background-image", "url(./images/天气_多云.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_多云.jpg)");
   } else if (weather === '小雪转多云') {
-    $('.instrument').css("background-image", "url(./images/天气_多云.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_多云.jpg)");
   } else if (weather === '多云') {
-    $('.instrument').css("background-image", "url(./images/天气_多云.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_多云.jpg)");
   } else if (weather === '阴转多云') {
-    $('.instrument').css("background-image", "url(./images/天气_多云.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_多云.jpg)");
   } else if (weather === '阵雪') {
-    $('.instrument').css("background-image", "url(./images/天气_下雪.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_下雪.jpg)");
   } else if (weather === '晴转小雪') {
-    $('.instrument').css("background-image", "url(./images/天气_下雪.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_下雪.jpg)");
   } else if (weather === '小雪') {
-    $('.instrument').css("background-image", "url(./images/天气_下雪.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_下雪.jpg)");
   } else if (weather === '多云转小雪') {
-    $('.instrument').css("background-image", "url(./images/天气_下雪.jpg)");
+    $('.#weatherpage').css("background-image", "url(./images/天气_下雪.jpg)");
   } else if (weather === '大雪转小雪') {
-    $('.instrument').css("background-image", "url(./images/天气_下雪.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_下雪.jpg)");
   } else if (weather === '大雪') {
-    $('.instrument').css("background-image", "url(./images/天气_下雪.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_下雪.jpg)");
   } else if (weather === '雾') {
-    $('.instrument').css("background-image", "url(./images/天气_雾天.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_雾天.jpg)");
   } else if (weather === '霾') {
-    $('.instrument').css("background-image", "url(./images/天气_霾.jpg)");
+    $('#weatherpage').css("background-image", "url(./images/天气_霾.jpg)");
   } else {
     alert('没有你要的信息')
   }
